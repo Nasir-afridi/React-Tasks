4bfa73217978bc25d40f9b24cde767e823d12314
Nasir-afridi <nasirwc05@gmail.com>
2025-06-24 14:29:18 +0500

    task-3 rendering the object list of the users

--- Tasks/src/App.jsx
+++ Tasks/src/App.jsx
@@ -16,17 +16,34 @@ function App() {
         setError('');
       }
       console.log(e)
-    } */
+    } 
 
-  const cars = ["toyota", "BMW", "suzuki", "honda"];
+    Task-2
+    const cars = ["toyota", "BMW", "suzuki", "honda"];*/
+
+    const users = [
+  { name: "Nasir", age: 19 },
+  { name: "Ali", age: 22 },
+];
   return (
     <>
+      <ul>
+        <h2>users</h2>
+        {users.map((user, index) => (
+          <li key={index}>Name: {user.name} , age: {user.age}</li>
+        ))} 
+      </ul>
+
+
+      {/* 
+      task-2
       <h2>cars list</h2>
       <ul>
         {cars.map(function (car, index) {
           return <li key={index}>{car}</li>;
         })}
-      </ul>
+      </ul> 
+      */}
 
       {/* 
       *****Task - 1
